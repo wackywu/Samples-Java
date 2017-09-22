82e5e867dc2e0a02b0c90cfeb5793f680e5ea22e
Vladimir <hotchenko@gmail.com>
2017-09-22 14:06:20 +0300

    update samples code

--- webfx/src/com/stimulsoft/ApplicationInitializer.java
+++ webfx/src/com/stimulsoft/ApplicationInitializer.java
@@ -16,23 +16,6 @@ public class ApplicationInitializer implements ServletContextListener {
     @Override
     public void contextInitialized(final ServletContextEvent event) {
         try {
-            /*com.stimulsoft.base.licenses.StiLicense.setKey("6vJhGtLLLz2GNviWmUTrhSqnOItdDwjBylQzQcAOiHmdTpNNLNYC5SmErlx8kalRj3Tpz3Q7n0KSIx5FeZwPIkl3Gm"
-                    + "MUWe0X9q3lDh4/An8Eal8Cdu9+cxt4MA+TELz2m3Z5VBBjN1/XDAO+GQPJ5yw92sJSJNVdYMFKnjzHDAICPHlRZrUP"
-                    + "+7byVoZqOYMMfas8oJiFavGjIbjlFG1BwhMNCKMa4H0f93ehhnZNBmH/lstCzBtojK/dwh5HtBSUV2nEIQ+kIz2m4m"
-                    + "Cher2PuoD1lANnArhoBkfdb1y7UiNvMK34Fb2pZpCiUdGP3aBC40aZpqILVSUQ79A4nVghyrc0DHOdtcp677HSGoR8"
-                    + "Qooezs9YJMJx8438LsQdzl1jhqkUc0W2euM5hbMf57RWjQHIJXyn6CmjBiXVNRDwKuikphRvZ4QKTaGpcxmsdDh0Aq"
-                    + "Q/PllbHvFjk1LG5UwhcfKOJET9M6keMF8xoUa5asm/puYil6o9XZd/zbrqQm7rYpoOLo2XF4u+YjX0hHP1hFtUiOhU"
-                    + "n0ekuMCv9GOr5JFqrJXEDoIKoHTr2TuGKRRP7wmz9qsU/Q+y8JP1o1a5TBqoBidVlVwSGEQnhcQUILgoGzB+RWCBgg"
-                    + "K1VrkpT38izvy5Xjh6fpMAPc7hr5p6IaHfr5oSVS1OqyWGxR/vgrzxx2rrCjIM4eNIiAQ4Tcy/lbTyI3UwOyysWi+P"
-                    + "+bX9lZ3NuzJix2CJb8EmbWHMIfhNAmLeP8EBqEoguFnaUjw46bNp4anPZgRVhQH+OauO9XMkaPfBMAXRk9ygM0v67s"
-                    + "qGLayeJ23rqPWTBi60NpB6jydFNkyV7zZmbnrkJExCQmi+zP5mUWEzOTYVsUyKccDOqpZ829/8+8GfcvrnbhGgYK+/"
-                    + "13wNfxmPz33DBAWzZjgC5pBFq72N8wRJq3qBHIcFA1WTxpgwa5coqEC3zCihz0lQeff+hde5JU+uXSOeIuTyi7MitK"
-                    + "r1X1a7P5+13ApOYy0xmdtpWnpO9O3uw0qQGH5uZi0Ha+3abjKNrWR+dPI8Kb6ZGTuYt22Wf4rJzPuj1uxc5pqT8MOi"
-                    + "+Lbkc8W+EGZ5VV5F2FAkAknhauv3q7Yg8cwPu3hH37rC/NTneH9z9eYAVtr+Yfcg6ljusEHM6dixwC+UCWzhh9Mg1c"
-                    + "hdgy0pZ//g2IVYUw1ncW2yHwfB5T27/CDnpgjXXnB0Uzt69+A+DYlQXi3V3r7QE8O2pqnc6v0YysOp72seIjQcbKTc"
-                    + "QyxlZWb17DNHnITQTHySzomqk77c0RKmqfqCuxjLmUG1peqRsCDyLV1jtZ9LyqPEKJhiIDezoXl5UOs4IiTeDCcUPK"
-                    + "JtKVa2qoDw6evYvAS7GtHsDe714czHjj9F1bue25274Sht7S8pzKVNv0oC");*/
-
             // configuration application
             StiFlexConfig stiConfig = initConfig();
             // Setup custom properties
